ac805aca6327701ef033636ee5381a3a82227057
feliciodeangela <feliciodeangela@gmail.com>
2022-09-08 15:59:55 -0700

    fix 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	printf("with propper grammar, but the outcome is a piece of art, \n");
+	printf("With propper grammar, but the outcome is a piece of art, \n");
 	return (0);
 }
