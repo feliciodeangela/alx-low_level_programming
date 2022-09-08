11f3b485220cc223704fc4218434d313e603b252
feliciodeangela <feliciodeangela@gmail.com>
2022-09-08 16:46:13 -0700

    fix try #6 fil 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ int main(void)
 	printf("Size of an int: %ld byte(s)\n", sizeof(a));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(c));
-	printf("size of a float: %ld byte(s)\n ", sizeof(f));
+	printf("Size of a float: %ld byte(s)\n ", sizeof(f));
 	return (0);
 }
