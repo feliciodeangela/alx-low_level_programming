70995b92aa1f46ae0f427fe142e85e46c38ae608
feliciodeangela <feliciodeangela@gmail.com>
2022-09-09 13:31:23 -0700

    up fix#2 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,15 @@
  * Return: Always 0 (Success/correct)
  */
 
-int main(void){
+int main(void)
+{
 	int number;
-	for(number=48;number<=57;number++){
+
+	for (number = 48 ; number <= 57 ; number++)
+	{
 		putchar(number);
-		if(number==57){
+		if (number == 57)
 			break;
-		}
 		putchar(',');
 		putchar(' ');
 	}
