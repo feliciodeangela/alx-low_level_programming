80fad989a2a4380e353b028c2159f083e7b8af9f
feliciodeangela <feliciodeangela@gmail.com>
2022-09-13 18:02:25 -0700

    up task 9 & 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: first argument
+ * @b: second argument
+ *
+ * Description: Adds two numbers
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
