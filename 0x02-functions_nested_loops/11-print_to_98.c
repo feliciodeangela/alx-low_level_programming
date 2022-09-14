ac8191eb5a1f35f12cf7c2face2f2cd3e8a32d77
feliciodeangela <feliciodeangela@gmail.com>
2022-09-13 18:44:03 -0700

    fix#2 task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,12 +12,12 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		for (n; n > 98; n--)
+		for (; n > 98; n--)
 			printf("%d, ", n);
 	}
 	else
 	{
-		for (n < 98; n< 98; n--)
+		for (; n< 98; n--)
 			printf("%d, ",n);
 	}
 }
