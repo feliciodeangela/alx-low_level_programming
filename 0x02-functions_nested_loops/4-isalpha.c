0d7f7ae2ab24f247e654543bc850d486a50eaddd
feliciodeangela <feliciodeangela@gmail.com>
2022-09-13 18:13:41 -0700

    fix task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,10 +10,10 @@
 
 int _isalpha(int c)
 {
-	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'))
+	if (('a' >= c && c <= 'z') || ('A' <= c && c <= 'Z'))
 	{
-		return (0);
+		return (1);
 	}
 	else
-		return (1);
+		return (0);
 }
