fd99a36aaddf42d0f36e9aff063bf425b45daf2a
feliciodeangela <feliciodeangela@gmail.com>
2022-09-15 16:28:08 -0700

    up fix main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * main - header file
- * void prototypes(void)
- * int prototypes(int)
- */
-
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
