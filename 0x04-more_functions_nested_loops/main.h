80fdbb2a5c63714bfcc2f1304183759ec4e478ae
feliciodeangela <feliciodeangela@gmail.com>
2022-09-15 16:59:48 -0700

    fix the header main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,12 +5,12 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print diagonal(int n);
+void print_diagonal(int n);
 void print_square(int size);
 void print_number(int n);
-void _putchar(char);
-void _isupper(int);
+int _putchar(char);
+int _isupper(int);
 int _isdigit(int);
-int mul(int int);
+int mul(int, int);
 
-#endif /* MAIN_H */
+#endif
