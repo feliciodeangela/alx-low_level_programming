7b822c58287d83a5da721a5054831984094b5809
feliciodeangela <feliciodeangela@gmail.com>
2022-09-19 18:39:19 -0700

    up task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * Description: prints elements of array
+ * @a: array to print
+ * @n: number of elements to print
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
