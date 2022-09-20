f87cd5068a3594e3fe0d5c4f8d9112ed5b273d1f
feliciodeangela <feliciodeangela@gmail.com>
2022-09-19 18:54:34 -0700

    fix task 6 + chmod

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
-void put2(char *str);
+void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+void *_strcpy(char *dest, char *src);
