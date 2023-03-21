99c620a8adff3ab9895b92c943403dc9764cac66
feliciodeangela <feliciodeangela@gmail.com>
2023-03-21 15:43:13 -0700

    up task 5 fix #1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,24 +11,15 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('/');
 		return (-1);
 	}
 	else if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('1');
 		return (1);
 	}
 	else
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('0');
 		return (0);
 	}
