feb1813b035bd6b94f198ba5e2d1ce8e010c9078
feliciodeangela <feliciodeangela@gmail.com>
2023-03-21 07:25:07 -0700

    up task task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "MAIN.H"
+/**
+ * main - Entry point.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ptchr[] {'_','p','u','t','c','h','a','r'};
+	int aSize = sizeof(ptchr) / sizeof(ptchr[0]);
+	int i = 0;
+
+	while (i < aSize)
+	{
+		_putchar(ptchr[i]);
+		i++;
+	}
+}
