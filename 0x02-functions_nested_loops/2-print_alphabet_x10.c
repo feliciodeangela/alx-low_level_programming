aafe875f77a31a3fe0c259d4d0d55141229a8845
feliciodeangela <feliciodeangela@gmail.com>
2023-03-21 10:44:44 -0700

    up main + task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet10x - Entry point.
+ * Description: Prints the alphabet ten times.
+ */
+void print_alphabet10x(void)
+{
+	char abcd[26];
+	char l = 'a'
+	int j, i = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+		while (i <= 26)
+		{
+			abcd[i] = l;
+			_putchar(abcd[i]);
+			l++;
+			i++;
+		}
+		_putchar('\n');
+	}
+}
