7f5f8c5ad28425ad113afd7e29decd7e5daa5a68
feliciodeangela <feliciodeangela@gmail.com>
2023-03-21 10:17:54 -0700

    up task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - Enty point.
+ * Description: Prints the alphabet.
+ * Return: Always 0 (Success).
+ */
+void print_alphabet(void)
+{
+	char abcd[27];
+	int i = 0;
+
+	while (i < 26)
+	{
+		abcd[i] = 'a' + i;
+		_putchar(i);
+		i++;
+	}
+	abcd[i] = '\n';
+	_putchar(abcd[i]);
+	return (0);
+}
