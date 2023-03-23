19950cfd9b70e8bacb04b052833628445ab522b2
feliciodeangela <feliciodeangela@gmail.com>
2023-03-23 14:31:25 -0700

    up main + task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Entry point.
+ * Description: multiplies two numbers.
+ * @a: integer to multiply.
+ * @b: integer to multiply.
+ * Return: Result of the multiplication of @a and @b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
