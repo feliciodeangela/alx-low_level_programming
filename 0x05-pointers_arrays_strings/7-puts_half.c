cf37389bf23afb3be0067ee03444a751b01ddc48
feliciodeangela <feliciodeangela@gmail.com>
2023-03-27 15:03:59 -0700

    up task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * Description: Prints half of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	
+}
