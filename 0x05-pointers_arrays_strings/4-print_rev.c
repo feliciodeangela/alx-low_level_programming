961253e64adcc9262c873940391eb86477122fb0
feliciodeangela <feliciodeangela@gmail.com>
2023-03-27 15:02:52 -0700

    up task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s: value to print
+ *
+ * Description: prints value in reverse
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
