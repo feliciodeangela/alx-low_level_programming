bc648168453d4a858a4e966dd1ff8aa38d2dfc8e
feliciodeangela <feliciodeangela@gmail.com>
2023-04-03 16:40:51 -0700

    up main + task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_memset - Entry point.
+ * Description:  fills the first @n bytes of the memory area pointed to by @s with the constant byte @b.
+ * @n: bytes to fill.
+ * @s: area of memory to fill.
+ * @b: character to fill.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return *s;
+}
