15bfa8ab6b19844fa59fb0727560f4182eafcd9f
feliciodeangela <feliciodeangela@gmail.com>
2023-04-04 15:50:38 -0700

    up task 5 fixx

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,14 +1,27 @@
 #include "main.h"
-
 /**
  * rev_string - Entry point
  * Description: reverse a string
  * @s: string to reverse
- *
- * Return: void
  */
-
 void rev_string(char *s)
 {
-	
+	int i = 0, j = sizeof(s) - 1;
+	char c[j];
+
+	while (i < j)
+	{
+		c[i] = *s;
+		i++;
+		s++;
+	}
+	i--;
+	s--;
+	while (i < j)
+	{
+		*s = c[i];
+		i++;
+		s--;
+	}
+
 }
