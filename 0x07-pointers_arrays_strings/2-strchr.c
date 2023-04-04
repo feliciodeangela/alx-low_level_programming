97dafc9510619d20b8fffebdd7e1b9792e90f3aa
feliciodeangela <feliciodeangela@gmail.com>
2023-04-03 18:19:58 -0700

    up main + task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - Locates a character.
+ * @c: character to locate.
+ * @s: string to search.
+ * Return: pointer to located character | Null
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s);
+		}
+		i++;
+	}
+	return (NULL);
+}
