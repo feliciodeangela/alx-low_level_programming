09d87ac67519d04b6503b9d4a83eab12d2f5ef7b
feliciodeangela <feliciodeangela@gmail.com>
2023-04-03 17:40:16 -0700

    up task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,19 @@
-#include "main.h"       
-
+#include "main.h"
 /**
- * *_memcpy -  copies memory area from src to dest
- * @dest: destination
+ * *_memcpy - Entry point.
+ * Description:  copies memory area from @src to @dest
+ * @dest: destinyu
  * @src: source
- * @n: max bytes to use
- * Return: dest
+ * @n: bytes to use
+ * Return: destiny
  */
-
-char *_memcpy(char *dest, char *src, unsigned int n)         
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; n > 0 ; i++, n--) 
-        {
-                dest[i] = src[i]; 
-        }
-        return (dest);
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
 }
