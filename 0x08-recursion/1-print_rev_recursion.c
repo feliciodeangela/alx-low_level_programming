db9ce05c9f28d20c993a605ebbc012acca8807a2
feliciodeangela <feliciodeangela@gmail.com>
2023-04-09 15:43:08 -0700

    up task 1 fix

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Entry point.
+ * Description: Print string in reverse.
+ * @s: String to print.
+ */
+void _print_rev_recursion(char *s)
+{
+	int end = 0;
+	int pEnd = &end;
+
+	if (end == 0)
+	{
+		if (*s != '\0')
+		{
+			*pEnd = 1;
+		}
+		s++;
+		_print_rev_recursion(s);
+	}
+	else
+	{
+		if (*s != '\n')
+		{
+			_putchar(*s);
+			s--;
+			_print_rev_recursion(s);
+		}
+		_putchar(*s);
+	}
+}
