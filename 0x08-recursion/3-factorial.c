ad222a55f1269b8d484716e4c4870461d177fd76
feliciodeangela <feliciodeangela@gmail.com>
2023-04-09 09:00:01 -0700

    up task 3 fixit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,12 +7,16 @@
  */
 int factorial(int n)
 {
-	if (n == 1)
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	if (n == 0)
 	{
 		return (1);
 	}
 	else
 	{
-		return (n - 1);
+		return (-1);
 	}
 }
