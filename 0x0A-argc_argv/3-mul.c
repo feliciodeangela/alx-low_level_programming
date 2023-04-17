f8d93cfed290aae166cf86b0705b4d92c5b4164d
feliciodeangela <feliciodeangela@gmail.com>
2023-04-16 18:01:36 -0700

    up task 3 fix #2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point.
  * Description: Print the result of the multiplication of two numbers.
@@ -10,14 +11,14 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc > 2)
+	if (argc > 3 && argc < 3)
 	{
 		printf("Error\n");
 		return (mul);
 	}
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		mul = mul * argv[i];
+		mul = mul * atoi(argv[i]);
 	}
 	printf("%d\n", mul);
 	return (0);
