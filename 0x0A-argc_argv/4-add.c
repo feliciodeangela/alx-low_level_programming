5a58b85b6586d99ceae9f92dd039b285a89010b9
feliciodeangela <feliciodeangela@gmail.com>
2023-04-16 18:13:41 -0700

    up task 4 + task 3 fix #3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,23 +2,28 @@
 #include <stdlib.h>
 /**
  * main - Entry point.
- * Description: Print the result of the multiplication of two numbers.
+ * Description: Print the result of the addition of two numbers.
  * @argc: number of arguments.
  * @argv: Array of arguments.
  * Return: Success 0 | Failure 1.
  */
 int main(int argc, char *argv[])
 {
-	int i, mul = 1;
+	int i, sum = 0;
 
-	if (argc != 3)
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else if (argv[1] < 0 || argv[2] < 0)
 	{
 		printf("Error\n");
-		return (mul);
+		return (1);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		mul = mul * atoi(argv[i]);
+		mul = mul + atoi(argv[i]);
 	}
 	printf("%d\n", mul);
 	return (0);
