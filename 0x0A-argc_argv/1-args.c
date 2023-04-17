3619ff9ea8e88d12ddb63682133794e805bd79cd
feliciodeangela <feliciodeangela@gmail.com>
2023-04-16 17:29:45 -0700

    up task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Print number of arguments.
+ * @argc: number of arguments.
+ * @argv: Array of arguments.
+ * rETURN: Success 0 | Failure 1.
+ */
+int main(int argc, char *argv[])
+{
+	argv++;
+	print("%d\n", argc--);
+	return (0);
+}
