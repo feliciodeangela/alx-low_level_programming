0a4e1279236528c4d40a0d3c27861048c4bea956
Felicio Avelino <felicio@debian>
2023-06-08 22:19:27 +0200

    up task 1 fix 6

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,12 +8,10 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		s++;
-		_print_rev_recursion(s);
+		_print_rev_recursion(s++);
 	}
 	if (*s != '\n')
 	{
-		s--;
-		_putchar(*s);
+		_putchar(*(s - 1));
 	}
 }
