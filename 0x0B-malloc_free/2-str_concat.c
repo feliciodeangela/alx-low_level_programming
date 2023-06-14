17bcf4013135a2fcf80a42f27fa8566af0f7543f
feliciodeangela <feliciodeangela@gmail.com>
2023-06-14 11:39:54 +0200

    up task 2 fix #4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,14 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k, n;
 	char *restr;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	while (*(s1 + i) != '\0')
 	{
 		i++;
