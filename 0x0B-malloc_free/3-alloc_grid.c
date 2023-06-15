950f7812688d06385765b5800a11bfb5199f82e0
feliciodeangela <feliciodeangela@gmail.com>
2023-06-15 23:18:10 +0200

    up task 3 + main

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * alloc_grid - Entry point.
+ * Description: Create a @width * @heigth array of integers.
+ * @width: number of collumns.
+ * @heigth: number of lines.
+ * Return: int **array.
+ */
+int **alloc_grid(int width, int heigth)
+{
+	int i, j;
+	int **grid;
+
+	if (width == 0 || heigth == 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(sizeof(int *) * heigth);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < heigth; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i] = malloc(sizeof(int) * width);
+		}
+	}
+	for (i = 0; i < heigth; i++)
+        {
+                for (j = 0; j < width; j++)
+                {
+                        grid[i][j] = 0;
+                }
+        }
+	return (grid);
+}
