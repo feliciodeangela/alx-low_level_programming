5ee44d6288be246808ab7d554da564367ec21c17
feliciodeangela <feliciodeangela@gmail.com>
2023-06-21 01:08:11 +0200

    up task 2 + dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+/**
+ * print_dog - Entry point
+ * Description: Print the dog struct.
+ * @d: Struct to print.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
