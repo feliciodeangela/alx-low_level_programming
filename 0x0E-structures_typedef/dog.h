3c700840e5553aa3b0837d0147aceeac2ec78d0c
feliciodeangela <feliciodeangela@gmail.com>
2023-06-21 01:33:09 +0200

    up dog + task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define DOG_H
 #include <stdio.h>
 #include <stdlib.h>
+#ifndef dog
 /**
  * struct dog - Represents a dog with a name, age, and owner.
  * @name: The name of the dog.
@@ -14,6 +15,15 @@ struct dog
 	float age;
 	char *owner;
 };
+#endif
+/**
+ * dog_t - Entry point.
+ * Description: Dog typedef.
+ * @name: Dog name.
+ * @age: Dog age.
+ * @owner: Dog owner.
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
