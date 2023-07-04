3b5666c21172ed23f7b37605e8655945afb3d22d
feliciodeangela <feliciodeangela@gmail.com>
2023-07-04 23:07:39 +0200

    up task 0, header & README

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - Print all elements of a list.
+ * @h: list element to print.
+ * Return: Number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned long int s = 0;
+	const listint_t *aux = h;
+
+	if (aux != NULL)
+	{
+		printf("[%u]\n", aux->n);
+		s = s + print_listint(aux->next);
+	}
+	return (s);
+}
