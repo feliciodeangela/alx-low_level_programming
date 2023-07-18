c52be32f566a363f9fc94b6ab8c5902c5c131720
feliciodeangela <feliciodeangela@gmail.com>
2023-07-18 22:25:50 +0200

    up task 1 & main finally!

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
-char *_strncpy(const char *dest, const char *src, int n);
+int create_file(const char *filename, char *text_content);
 
 #endif
