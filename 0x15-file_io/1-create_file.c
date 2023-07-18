5156bc60870be045cc105bdaa0e67e530a0a1d66
feliciodeangela <feliciodeangela@gmail.com>
2023-07-19 00:02:38 +0200

    up task 1 fix #4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,9 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fileDesc = open(filename, O_WRONLY | O_TRUNC);
-	if (fileDesc < 0)
-		fileDesc = open(filename, O_WRONLY | O_CREAT, 0600);
+	fileDesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fileDesc < 0)
 		return (-1);
 	if (text_content == NULL)
